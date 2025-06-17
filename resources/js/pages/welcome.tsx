d1e9843df61c7c1ab96cd83632677c442f53baeb
James Minton <james7minton@gmail.com>
2025-06-17 22:33:03 +0100

    Another quick test

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -38,10 +38,7 @@ export default function Welcome() {
                         )}
                     </nav>
                 </header>
-                <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-                    <p>Quick Test for automatic push to laravel forge</p>
-                    <img src="/images/yarakubooks.png" />
-                </div>
+                <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0"></div>
                 <div className="hidden h-14.5 lg:block"></div>
             </div>
         </>
